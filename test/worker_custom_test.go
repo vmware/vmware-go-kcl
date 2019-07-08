5d9ddeae8916f82edbf3f862ee58b64872979d84
Tao Jiang <taoj@vmware.com>
2019-07-08 17:20:33 -0500

    Update worker to let it inject checkpointer and kinesis (#28)
    * Update worker to let it inject checkpointer and kinesis
    
    Add two functions to inject checkpointer and kinesis for custom
    implementation or adding mock for unit test.
    
    This change also remove the worker_custom.go since it is no longer
    needed.
    
    Test:
      Update the integration tests to cover newly added functions.
    
    Signed-off-by: Tao Jiang <taoj@vmware.com>
    
    * Fix typo on the test function
    
    Signed-off-by: Tao Jiang <taoj@vmware.com>

--- test/worker_custom_test.go
+++ test/worker_custom_test.go
@@ -23,6 +23,10 @@ import (
 	"testing"
 	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kinesis"
+
 	log "github.com/sirupsen/logrus"
 
 	"github.com/stretchr/testify/assert"
@@ -32,7 +36,7 @@ import (
 	wk "github.com/vmware/vmware-go-kcl/clientlibrary/worker"
 )
 
-func TestCustomWorker(t *testing.T) {
+func TestWorkerInjectCheckpointer(t *testing.T) {
 	kclConfig := cfg.NewKinesisClientLibConfig("appName", streamName, regionName, workerID).
 		WithInitialPositionInStream(cfg.LATEST).
 		WithMaxRecords(10).
@@ -51,11 +55,112 @@ func TestCustomWorker(t *testing.T) {
 	// configure cloudwatch as metrics system
 	metricsConfig := getMetricsConfig(kclConfig, metricsSystem)
 
+	// custom checkpointer or a mock checkpointer.
 	checkpointer := chk.NewDynamoCheckpoint(kclConfig)
 
-	worker := wk.NewCustomWorker(recordProcessorFactory(t), kclConfig, checkpointer, metricsConfig)
+	// Inject a custom checkpointer into the worker.
+	worker := wk.NewWorker(recordProcessorFactory(t), kclConfig, metricsConfig).
+		WithCheckpointer(checkpointer)
 
 	err := worker.Start()
+	assert.Nil(t, err)
+
+	// Put some data into stream.
+	for i := 0; i < 100; i++ {
+		// Use random string as partition key to ensure even distribution across shards
+		err := worker.Publish(streamName, utils.RandStringBytesMaskImpr(10), []byte(specstr))
+		if err != nil {
+			t.Errorf("Errorin Publish. %+v", err)
+		}
+	}
+
+	// wait a few seconds before shutdown processing
+	time.Sleep(10 * time.Second)
+	worker.Shutdown()
+}
+
+func TestWorkerInjectKinesis(t *testing.T) {
+	kclConfig := cfg.NewKinesisClientLibConfig("appName", streamName, regionName, workerID).
+		WithInitialPositionInStream(cfg.LATEST).
+		WithMaxRecords(10).
+		WithMaxLeasesForWorker(1).
+		WithShardSyncIntervalMillis(5000).
+		WithFailoverTimeMillis(300000).
+		WithMetricsBufferTimeMillis(10000).
+		WithMetricsMaxQueueSize(20)
+
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.DebugLevel)
+
+	assert.Equal(t, regionName, kclConfig.RegionName)
+	assert.Equal(t, streamName, kclConfig.StreamName)
+
+	// configure cloudwatch as metrics system
+	metricsConfig := getMetricsConfig(kclConfig, metricsSystem)
+
+	// create custom Kinesis
+	s, err := session.NewSession(&aws.Config{
+		Region: aws.String(regionName),
+	})
+	assert.Nil(t, err)
+	kc := kinesis.New(s)
+
+	// Inject a custom checkpointer into the worker.
+	worker := wk.NewWorker(recordProcessorFactory(t), kclConfig, metricsConfig).
+		WithKinesis(kc)
+
+	err = worker.Start()
+	assert.Nil(t, err)
+
+	// Put some data into stream.
+	for i := 0; i < 100; i++ {
+		// Use random string as partition key to ensure even distribution across shards
+		err := worker.Publish(streamName, utils.RandStringBytesMaskImpr(10), []byte(specstr))
+		if err != nil {
+			t.Errorf("Errorin Publish. %+v", err)
+		}
+	}
+
+	// wait a few seconds before shutdown processing
+	time.Sleep(10 * time.Second)
+	worker.Shutdown()
+}
+
+func TestWorkerInjectKinesisAndCheckpointer(t *testing.T) {
+	kclConfig := cfg.NewKinesisClientLibConfig("appName", streamName, regionName, workerID).
+		WithInitialPositionInStream(cfg.LATEST).
+		WithMaxRecords(10).
+		WithMaxLeasesForWorker(1).
+		WithShardSyncIntervalMillis(5000).
+		WithFailoverTimeMillis(300000).
+		WithMetricsBufferTimeMillis(10000).
+		WithMetricsMaxQueueSize(20)
+
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.DebugLevel)
+
+	assert.Equal(t, regionName, kclConfig.RegionName)
+	assert.Equal(t, streamName, kclConfig.StreamName)
+
+	// configure cloudwatch as metrics system
+	metricsConfig := getMetricsConfig(kclConfig, metricsSystem)
+
+	// create custom Kinesis
+	s, err := session.NewSession(&aws.Config{
+		Region: aws.String(regionName),
+	})
+	assert.Nil(t, err)
+	kc := kinesis.New(s)
+
+	// custom checkpointer or a mock checkpointer.
+	checkpointer := chk.NewDynamoCheckpoint(kclConfig)
+
+	// Inject both custom checkpointer and kinesis into the worker.
+	worker := wk.NewWorker(recordProcessorFactory(t), kclConfig, metricsConfig).
+		WithKinesis(kc).
+		WithCheckpointer(checkpointer)
+
+	err = worker.Start()
 	assert.Nil(t, err)
 
 	// Put some data into stream.
